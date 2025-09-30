6b41b4277b679eac8681bd940ae076098a1b9fbb
Son Paul <paul.angeles.chaquire@gmail.com>
2025-09-30 19:17:02 +0200

    upd js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 // === Variablen ===
 let POKE_API_URL = "https://pokeapi.co/api/v2/pokemon";
-let POKE_API_LIMIT = 20; // kann man anpassen
+let POKE_API_LIMIT = 20;
 let POKE_API_OFFSET = 0;
-let SPRITE_DEFAULT =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+let SPRITE_DEFAULT = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-// DOM Ablage in der Main Sektion
 let pokemonList = document.getElementById("pokemon-list");
-
-let fetchURL =
-  POKE_API_URL + `?limit=${POKE_API_LIMIT}&offset=${POKE_API_OFFSET}`;
+let fetchURL = POKE_API_URL + `?limit=${POKE_API_LIMIT}&offset=${POKE_API_OFFSET}`;
 
 async function fetchPokemonData() {
   try {
@@ -20,91 +16,48 @@ async function fetchPokemonData() {
       return;
     }
     let pokeArray = requestedDataAsJson.results;
-    renderPokemons(pokeArray);
-    console.log(pokeArray);
+    
+    // Für jedes Pokemon die Details holen
+    for (let i = 0; i < pokeArray.length; i++) {
+      let detailsResponse = await fetch(pokeArray[i].url);
+      let pokemonDetails = await detailsResponse.json();
+      renderPokemon(pokemonDetails, i);
+    }
+    
+    console.log("Alle Pokemon geladen!");
   } catch (error) {
     console.log(error);
   }
 }
 
 fetchPokemonData();
-// 'pokemons' dient hier als Platzhalter für 'pokeArray'
-function renderPokemons(pokemons) {
-  for (let i = 0; i < pokemons.length; i++) {
-    let pokeName = pokemons[i].name.toUpperCase();
-    let pokeIndex = i + POKE_API_OFFSET + 1; // Index startet bei 1
-    pokemonList.innerHTML += pokemonCardTemplate(pokeName, pokeIndex);
+
+function renderPokemon(pokemon, index) {
+  let pokeName = pokemon.name.toUpperCase();
+  let pokeIndex = index + POKE_API_OFFSET + 1;
+  
+  // Typen aus dem Array extrahieren
+  let typesHTML = "";
+  for (let i = 0; i < pokemon.types.length; i++) {
+    let typeName = pokemon.types[i].type.name.toUpperCase();
+    typesHTML += `<div class="pokemon-card-type">${typeName}</div>`;
   }
+  
+  pokemonList.innerHTML += pokemonCardTemplate(pokeName, pokeIndex, typesHTML);
 }
 
-function pokemonCardTemplate(name, index) {
+function pokemonCardTemplate(name, index, typesHTML) {
   let spriteUrl = SPRITE_DEFAULT + index + ".png";
   return `<div class="pokemon-card">
-  <div class="pokemon-card-sprite">
-  <img src="${spriteUrl}" alt="${name}" />
-  </div>
-  <div class="pokemon-card-info">
-    <p>#${index}</p>
-    <h2>${name}</h2>
-    <div class="pokemon-card-types">
-        <div class="pokemon-card-type>Typ 1</div>
-        <div class="pokemon-card-type>Typ 2</div>
+    <div class="pokemon-card-sprite">
+      <img src="${spriteUrl}" alt="${name}" />
     </div>
-  </div>
-</div>`;
+    <div class="pokemon-card-info">
+      <p>#${index}</p>
+      <h2>${name}</h2>
+      <div class="pokemon-card-types">
+        ${typesHTML}
+      </div>
+    </div>
+  </div>`;
 }
-
-// function pokemonCardTemplate(name, index) {
-//   return `<div class="pokemon-card">
-//   <div class="pokemon-card-sprite">
-//     <img src="${spriteUrl}" alt="${name}" />
-//   </div>
-//   <div class="pokemon-card-info">
-//     <p>#${index}</p>
-//     <h2>${name}</h2>
-//     <div class="pokemon-card-types">
-//         <div>Typ 1</div>
-//         <div>Typ 2</div>
-//     </div>
-//   </div>
-// </div>`;
-// }
-
-// // === INIT ===
-// async function init() {
-//   let pokemons = await fetchPokemons();
-//   renderPokemons(pokemons);
-// }
-// init();
-
-// // == FETCH ==
-// async function fetchPokemons() {
-//   let url = POKE_API_URL + `?limit=${POKE_API_LIMIT}&offset=${POKE_API_OFFSET}`;
-//   let data = await fetch(url);
-//   let dataAsJson = await data.json();
-//   if (!data.ok) {
-//     console.error("api error");
-//     return [];
-//   }
-//   return dataAsJson.results;
-// }
-
-// // === RENDER ===
-// // Schrittweise anhängen: innerHTML += ...
-// function renderPokemons(pokemons) {
-//   for (let i = 0; i < pokemons.length; i++) {
-//     let poke = pokemons[i];
-//     let pokeIndex = i + POKE_API_OFFSET + 1; // Index startet bei 1
-//     pokemonList.innerHTML += pokemonCardTemplate(pokemons[i].name, pokeIndex);
-//   }
-// }
-
-// // === TEMPLATE ===
-// function pokemonCardTemplate(name, index) {
-//   let spriteUrl = SPRITE_DEFAULT + index + ".png";
-//   return `<div class="pokemon-card">
-//        <h2>#${index} ⏐ ${name}</h2>
-//        <img src="${spriteUrl}" alt="${name}">
-//        <p>TYP: WIP</p>
-//      </div>`;
-// }
